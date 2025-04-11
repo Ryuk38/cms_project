731ff4cc531f589e2f87e2a7cb9ad80181563996
Ryuk38 <c380bineesh@gmail.com>
2025-04-11 16:39:07 +0530

    Update index.php to use MYSQL_PORT 3307 as default

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-$servername = getenv('MYSQL_HOST') ?: "172.17.0.2";
+$servername = getenv('MYSQL_HOST') ?: "172.21.0.2"; // Fallback IP
 $username = getenv('MYSQL_USER') ?: "root";
 $password = getenv('MYSQL_PASSWORD') ?: "rootpassword";
 $dbname = getenv('MYSQL_DATABASE') ?: "cms_db";
-$port = getenv('MYSQL_PORT') ?: 3307;
+$port = getenv('MYSQL_PORT') ?: 3307; // Default to 3307 to match host port
 
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
